82e9b1644dda203b435d0ecdf18ead6ee05392bc
Aleks <Aleks_Kravchina@OutLook.com>
2019-02-11 00:23:31 +0400

    без коментариев.

--- src/com/greffort/buildings/net/client/SerialClient.java
+++ src/com/greffort/buildings/net/client/SerialClient.java
@@ -1,7 +1,61 @@
 package com.greffort.buildings.net.client;
 
+import com.greffort.buildings.Buildings;
+import com.greffort.interfaces.Building;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 /**
  * @author SerialClient
  */
 public class SerialClient {
+    public static void main(String[] args) {
+
+        try (
+                Socket socket = new Socket(InetAddress.getLocalHost(), 6666);
+                FileReader fileReader = new FileReader("1.txt");
+                ObjectOutputStream outD = new ObjectOutputStream(new DataOutputStream(socket.getOutputStream()));
+                ObjectInputStream inD = new ObjectInputStream(new DataInputStream(socket.getInputStream()));
+                Scanner scanner = new Scanner(new FileReader("InputTypeBuilding"));
+                PrintStream printStream = new PrintStream(new FileOutputStream(new File("OUTPUT")))) {
+
+            String lineType;
+            Building building;
+
+            while ((building = Buildings.readBuilding(fileReader)) != null && scanner.hasNextLine() && !socket.isOutputShutdown()) {
+                lineType = scanner.nextLine();
+                if (lineType.equals("")) {
+                    continue;
+                }
+                outD.writeUTF(lineType);
+                outD.flush();
+                System.out.println();
+                System.out.println("Тип здания - " + lineType);
+                System.out.println("Описание здания - " + building.toString());
+                System.out.println("Тип здания отправлен на сервер");
+
+                outD.writeObject(building);
+                outD.flush();
+                System.out.println("Здание отправлено на сервер");
+
+                Object report = inD.readObject();
+                System.out.println("Стоимость здания: " + report);
+
+                printStream.println(report);
+                System.out.println("Оценка записана в файл");
+            }
+            outD.writeUTF("exit");
+            outD.flush();
+        } catch (UnknownHostException e) {
+            e.getStackTrace();
+        } catch (IOException e) {
+            e.getStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.getStackTrace();
+        }
+    }
 }
--- src/com/greffort/buildings/net/server/sequental/SerialServer.java
+++ src/com/greffort/buildings/net/server/sequental/SerialServer.java
@@ -9,10 +9,9 @@ import com.greffort.exception.BuildingUnderArrestException;
 import com.greffort.factory.DwellingFactory;
 import com.greffort.factory.HotelFactory;
 import com.greffort.factory.OfficeFactory;
+import com.greffort.interfaces.Building;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Random;
@@ -24,35 +23,35 @@ import java.util.Scanner;
  * исключение передаётся клиенту в виде объекта и т.д.
  */
 public class SerialServer {
-    public static void main(String[] ar) {
-        int port = 6666;
-        try (ServerSocket serverSocket = new ServerSocket(port);
-             Socket socket = serverSocket.accept();
-             DataInputStream in = new DataInputStream(socket.getInputStream());
-             DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
+    public static void main(String[] arg) {
 
+        try (ServerSocket serverSocket = new ServerSocket(6666);
+             Socket socket = serverSocket.accept();
+             ObjectInputStream in = new ObjectInputStream(new DataInputStream(socket.getInputStream()));
+             ObjectOutputStream out = new ObjectOutputStream(new DataOutputStream(socket.getOutputStream()))) {
             System.out.println("Клиент подключился");
 
-            String line;
-            String type;
-            String report;
+            Object report;
             while (!socket.isClosed()) {
-                type = in.readUTF();
-                System.out.println("Получилди тип здания - " + type);
+                String type = in.readUTF();
 
                 if (type.equals("exit")) {
-                    System.out.println("Передача данных завершена");
+                    System.out.println("Передача данных завершена"+"\n");
                     break;
                 }
 
-                line = in.readUTF();
-                System.out.println("Получили данные о здании - " + line);
+                System.out.println("Получилди тип здания - " + type);
+                System.out.println();
 
-                report = valuationBuilding(type, line);
+                Building building = (Building) in.readObject();
+                System.out.println("Получили данные о здании - " + building.toString());
+
+                report = valuationBuilding(type, building);
                 System.out.println("Вычислена стоимость здания - " + report);
 
-                out.writeUTF(report);
+                out.writeObject(report);
                 out.flush();
+
                 System.out.println("Стоимость здания отправлена на клиент");
             }
         } catch (Exception x) {
@@ -60,30 +59,28 @@ public class SerialServer {
         }
     }
 
-    private static String valuationBuilding(String type, String in) {
-        try (Scanner scanner = new Scanner(in)) {
+    private static Object valuationBuilding(String type, Building building) {
+        try {
             arrest();
-            int number = 0;
-//            if (type.equals("office")) {
-//                Buildings.setBuildingFactory(new OfficeFactory());
-//                number = 1000;
-//            } else if (type.equals("dwelling")) {
-//                Buildings.setBuildingFactory(new DwellingFactory());
-//                number = 1500;
-//            } else {
-//                Buildings.setBuildingFactory(new HotelFactory());
-//                number = 2000;
-//            }
-            return String.valueOf(number * Buildings.readBuilding(scanner).getTotalSquare());
-        } catch (IOException e) {
-            System.out.println(e);
-        } catch (BuildingUnderArrestException e){
-            return "Здание под арестом";
+            double number = 0;
+            if (type.equals("dwelling")) {
+                Buildings.setBuildingFactory(new OfficeFactory());
+                number = 1000;
+            } else if (type.equals("office")) {
+                Buildings.setBuildingFactory(new DwellingFactory());
+                number = 1500;
+            } else {
+                System.out.println();
+                System.out.println();
+                Buildings.setBuildingFactory(new HotelFactory());
+                number = 2000;
+            }
+            return String.valueOf(building.getTotalSquare() * number);
+        } catch (BuildingUnderArrestException e) {
+            return e;
         }
-
-        return "Ошибка";
-//        return null;
     }
+
 
     private static void arrest() throws BuildingUnderArrestException {
         Random random = new Random();
